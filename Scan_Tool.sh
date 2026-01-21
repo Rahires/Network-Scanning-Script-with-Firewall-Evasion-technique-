b09438cbe8d10a43124b04b15d932a06d595c358
Sahebrao Shivaji Rahire <rahires818@gmail.com>
2026-01-21 10:21:09 +0530

    Update Scan_Tool.sh

--- Scan_Tool.sh
+++ Scan_Tool.sh
@@ -3,10 +3,29 @@
 # ==========================================================
 # Project Title : Network Scanning Tool
 # Degree        : B.Sc Final Year Project
-# Author        : Sahebrao Rahire 
+# Author        : Sahebrao Rahire
 # Description   : Real-time Nmap scanning with validation,
 #                 auto-installation, reporting, colors,
 #                 and progress visualization.
+# ==========================================================
+#
+# Copyright (c) 2026 Sahebrao Rahire
+#
+# This project makes use of Nmap (https://nmap.org/).
+# Nmap is licensed under the GNU General Public License v2 (GPLv2).
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 # ==========================================================
 
 # ------------------------------
